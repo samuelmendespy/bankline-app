6668f30bee41df415cbd13a0cb6449ae8da22002
pysmendes <75790706+pysmendes@users.noreply.github.com>
2022-05-18 12:46:27 -0300

    SH Bankline updated with search filter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MovimentacaoNewComponent } from './component/movimentacao-new/movimentacao-new.component';
+import { MovimentacaoListComponent } from './component/movimentacao-list/movimentacao-list.component';
+import { CorrentistaComponent } from './component/correntista/correntista.component';
+
+
+const routes: Routes = [
+
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
+  { path: 'movimentacoes', component: MovimentacaoListComponent },
+  { path: 'correntistas', component: CorrentistaComponent },
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
